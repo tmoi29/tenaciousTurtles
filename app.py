aeae22f1f3c36c1f723a64c60a151fc3fb60d5b3
Karina Ionkina <kionkina@stuy.edu>
2017-12-26 14:36:19 -0500

    login, create account

--- app.py
+++ app.py
@@ -4,7 +4,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template("index.html", login = False)
+
+@app.route('/login')
+def login():
+    return render_template("login.html", login = False)
+
+@app.route('/create_account')
+def c_a():
+    return render_template("create_account.html", login = False)
 
 
 if __name__ == '__main__':
