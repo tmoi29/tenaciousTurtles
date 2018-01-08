e88e0e6553b3324bb4d926952a60c435f21adcfa
Khyber Sen <kkysen@gmail.com>
2018-01-08 15:26:30 -0500

    Changed STRING columns to TEXT in setup_db.py.
    Deleted freegeoip.py since it's implemented in used in welp.js.
    
    Added requirements.txt with dependencies that can be installed
    by running pip install -r requirements.txt.

--- utils/setup_db.py
+++ utils/setup_db.py
@@ -1,12 +1,25 @@
 import sqlite3
 
+
 def initialize():
     db = sqlite3.connect("data/database.db")
-    c  = db.cursor()
+    c = db.cursor()
     
-    c.execute("CREATE TABLE IF NOT EXISTS accounts(username STRING PRIMARY KEY, password STRING)")
-    c.execute("CREATE TABLE IF NOT EXISTS reviews(restaurant INTEGER, user STRING, rating INTEGER, reviewTitle STRING, reviewContent STRING)")
+    c.execute('CREATE TABLE IF NOT EXISTS '
+              'accounts('
+              'username TEXT PRIMARY KEY, '
+              'password TEXT)')
+    
+    c.execute('CREATE TABLE IF NOT EXISTS '
+              'reviews('
+              'restaurant INTEGER, '
+              'user TEXT, '
+              'rating INTEGER, '
+              'reviewTitle TEXT, '
+              'reviewContent TEXT)')
+    
     db.commit()
     db.close()
+
 
 initialize()
